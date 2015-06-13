5acbf346a6cc6de8f88be34b09935b3118d299c7
ionneto <ionneto@gmail.com>
2015-06-12 23:10:29 -0300

    #218 palidromic numbers

--- palindromic_218.c
+++ palindromic_218.c
@@ -1,41 +1,42 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+	#include <stdio.h>
+	#include <stdlib.h>
 
-int reverse(int a) {
-	int aux;
-	int rev = 0;
-	while(a > 0){
-		aux = a%10;
-		rev = rev*10 + aux;
-		a = a/10;
+	unsigned long long reverse(unsigned long long a) {
+		unsigned long long aux;
+		unsigned long long rev = 0;
+		while(a > 0){
+			aux = a%10;
+			rev = rev*10 + aux;
+			a = a/10;
+		}
+		return rev;
 	}
-	return rev;
-}
 
-int main() {
-	int cont;
-	int input;
-	int rev;
-	int original;
+	int main() {
+		unsigned long long cont;
+		unsigned long long input;
+		unsigned long long rev;
+		unsigned long long original;
 
-	scanf("%d", &input);
-	rev = reverse(input);
-	original = input;
+		scanf("%d", &input);
+		rev = reverse(input);
+		original = input;
 
-	if(rev == input) {
-		printf("The input was already palindromic\n");
+		if(rev == input) {
+			printf("The input was already palindromic\n");
+			return 0;
+		}
+
+		while(rev!=input) {
+			input = rev + input;
+			rev = reverse(input);
+			cont ++;
+
+		//printf("%d = %d\n",input, rev );
+		}
+
+		printf("%lu is the palindromic of %lu, it took %lu sums with the reverse.\n", input, original, cont);
+
+
 		return 0;
 	}
-
-	while(rev!=input) {
-		input = rev + input;
-		rev = reverse(input);
-		cont ++;
-	}
-
-	printf("%d is the palindromic of %d, it took %d sums with the reverse.\n", input, original, cont);
-
-
-	return 0;
-}
